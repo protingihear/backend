e71767505dfe12d97106bf39aa42a0b34e29bfa9
xall-gitautoco <xallgitautopilot0@gmail.com>
2024-12-23 12:02:58 +0700

    make seed for teman-tuli, update komentar migrations, update timestamp migrations for komentar and postigan_relations

--- database/migrations/2024_01_01_000005_create_komentar_table.php
+++ database/migrations/2024_01_01_000005_create_komentar_table.php
@@ -9,16 +9,17 @@ class CreateKomentarTable extends Migration
     public function up()
     {
         Schema::create('komentar', function (Blueprint $table) {
-          $table->id('idKomentar'); // Primary Key
-    $table->string('isiKomentar', 255);
-    $table->unsignedBigInteger('idKomunitas'); // Foreign Key
-    $table->unsignedBigInteger('idTemanTuli'); // Foreign Key
-    $table->unsignedBigInteger('idTemanDengar'); // Foreign Key
-    $table->timestamps();
+            $table->id('idKomentar'); // Primary Key
+            $table->string('isiKomentar', 255);
+            $table->unsignedBigInteger('idPostingan'); // Foreign Key
+            $table->unsignedBigInteger('idTemanTuli'); // Foreign Key
+            // $table->unsignedBigInteger('idTemanDengar'); // Foreign Key
+            $table->timestamps();
+            $table->softDeletes();
 
-    $table->foreign('idKomunitas')->references('idKomunitas')->on('komunitas')->onDelete('cascade');
-    $table->foreign('idTemanTuli')->references('idTemanTuli')->on('teman_tuli')->onDelete('cascade');
-    $table->foreign('idTemanDengar')->references('idTemanDengar')->on('teman_dengar')->onDelete('cascade'); 
+            $table->foreign('idPostingan')->references('id')->on('postingan_relations')->onDelete('cascade');
+            $table->foreign('idTemanTuli')->references('idTemanTuli')->on('teman_tuli')->onDelete('cascade');
+            // $table->foreign('idTemanDengar')->references('idTemanDengar')->on('teman_dengar')->onDelete('cascade');
         });
     }
 
